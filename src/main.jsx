3dff27652045acb8b34be4139ff0ddc3277cbdb2
Ashwin Aggarwal <aka96@cornell.edu>
2026-01-02 14:36:52 -0500

    Fix: Correct StockDashboard import capitalization

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import StockDashboard from './components/stockdashboard'
+import StockDashboard from './components/StockDashboard'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
